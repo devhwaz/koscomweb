297bf7041ec918112a8cc65dbf0a0a7a50245717
carami <carami@nate.com>
2018-07-03 16:23:08 +0900

    web_basic

---
+++ src/kr/co/koscom/dao/MemberDAO.java
@@ -0,0 +1,11 @@
+package kr.co.koscom.dao;
+
+import java.util.List;
+
+import kr.co.koscom.dto.MemberDTO;
+
+public interface MemberDAO {
+	public int addMember(MemberDTO member);
+	public MemberDTO getMember(String id);
+	public List<MemberDTO> getMemberList();
+}
